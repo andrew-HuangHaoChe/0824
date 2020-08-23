82e7d5840c1bb3be91eb2f9eb24736bbc33d54aa
Huang Hao Che <62294412+andrew-HuangHaoChe@users.noreply.github.com>
2020-08-24 00:48:46 +0800

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <HomeNav></HomeNav>
+        <HomeHeader></HomeHeader>
+        <HomeContent></HomeContent>
+    </div>
+</template>
+<script>
+    import HomeNav from './HomeNavbar';
+    import HomeHeader from './HomeHeader';
+    import HomeContent from './HomeContent';
+    export default {
+        components:{
+            HomeNav,
+            HomeHeader,
+            HomeContent,
+        },
+    }
+</script>
---
+++ src/components/HomeNavbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a href="#">
+                    <img src="https://github.com/andrew-HuangHaoChe/image/blob/master/keyboardlogo.png?raw=true" class="logoImg">
+                </a>
+                <ul class="list-unstyled d-flex w-100 justify-content-end align-items-center mb-0">
+                    <li class="mr-5"><a href="#"><i class="fas fa-home fa-2x d-flex justify-content-center homepageicon"></i><div class="homeFont">首頁</div></a></li>
+                    <li class="mr-5"><a href="#"><i class="fas fa-ticket-alt fa-2x d-flex justify-content-center"></i><div class="icon">優惠活動</div></a></li>
+                    <li class="mr-5"><a href="#"><i class="fas fa-tasks fa-2x d-flex justify-content-center"></i><div class="icon">管理商品</div></a></li>
+                    <li class="mr-5"><router-link to="/login"><i class="far fa-user fa-2x d-flex justify-content-center"></i><div class="icon">登入</div></router-link></li>
+                </ul>
+            </nav>
+        </div>    
+    </div>
+</template>
